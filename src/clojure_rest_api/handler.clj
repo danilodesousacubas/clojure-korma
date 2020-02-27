932894b0fc223729ad2837ecfea2fc0dcf57a6d5
Danilo de Souza <danilodesousa@gmail.com>
2020-02-27 09:05:16 -0300

    Created store func to create,update and delete

--- src/clojure_rest_api/handler.clj
+++ src/clojure_rest_api/handler.clj
@@ -5,9 +5,25 @@
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]))
 
 (defroutes all-routes
-  (GET "/hello" [] "Hi")
   (GET "/stores" [] 
     (store/find-all))
+  (GET "/stores/:id" [id] 
+    (store/find-by-id id))
+  (POST "/stores" req
+    (let [name (get-in req [:body "name"])
+          email (get-in req [:body "email"])
+          codigo (get-in req [:body "codigo"])]
+          (store/create name email codigo)))
+  (DELETE "/stores/:id" [id]
+    (store/delete-by-id id)
+    (str "deleted store id " id))
+  (PUT "/stores/:id" req
+    (let [id (read-string (get-in req [:params :id]))
+          name (get-in req [:body "name"])
+          email (get-in req [:body "email"])
+          codigo (get-in req [:body "codigo"])]
+          (store/update-by-id id name email codigo)
+          (store/find-by-id id)))
   (route/not-found "Not Found"))
 
 (def app
--- src/clojure_rest_api/store.clj
+++ src/clojure_rest_api/store.clj
@@ -6,10 +6,22 @@
 (defentity store)
 
 (defn find-all []
-    (select store))
+  (select store))
 
 (defn find-by-id [id]
-    (select store
-      (where {:id id})
-      (limit 1)))
+  (select store
+    (where {:id id})
+    (limit 1)))
 
+(defn create [name email codigo]
+  (insert store
+    (values {:name name :email email :codigo codigo})))
+
+(defn delete-by-id [id]
+  (delete store
+    (where {:id id})))
+
+(defn update-by-id [id name email codigo]
+  (update store 
+    (set-fields {:name name :email email :codigo codigo})
+      (where {:id id})))
